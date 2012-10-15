e0d3adf6b475f50334957a8a4f2284cef13bc9f5
EC2 Default User <ec2-user@domU-12-31-39-04-49-CB.compute-1.internal>
2012-10-15 02:45:10 +0000

    change port

--- src/cheapec2.py
+++ src/cheapec2.py
@@ -25,5 +25,5 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    application.listen(8888)
+    application.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
